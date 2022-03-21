3177de55095c011e05d2304a62cb19347b275faf
Rhysdabeast <86208560+Rhysdabeast@users.noreply.github.com>
2022-03-21 03:29:23 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Realtime-Trains-Scraper
+A very simple scraper for https://www.realtimetrains.co.uk/ which exports to a local database file!
+
+# Packages
+Packages you need in order to run the program: <br />
+Requests - https://pypi.org/project/requests/ <br />
+Beautifulsoup4 - https://pypi.org/project/beautifulsoup4/ <br />
+Pandas - https://pypi.org/project/pandas/ <br />
+
+# Running the Program
+First of all clone this repository or download it in ZIP format. 
+From the project folder you can open the command prompt and type: <br />
+<code>python RTT_Scraper_Main.py</code> <br />
+<br />
+![RTT CMD Example](https://user-images.githubusercontent.com/86208560/159197557-c8fec7dd-f97f-4284-af71-f4675ae91545.gif)<br />
+
+Or you can open the project folder in Visual Studio Code or your prefered IDE and run the program through that:<br />
+https://code.visualstudio.com/
+
+# Using the Program
+
+The program is very simple to use, when you get the program running it will ask for a station name which you would like to scrape services from. To make sure you enter the correct name look inside the excel spreadsheet and copy the station name from that if necessary. This would be useful if you wanted to get services from Acton Bridge for example, if Acton Bridge was inputted a database file would be created but with no data inside of it. Instead you would have to input Acton Bridge (Cheshire) as that is what corresponds to the correct CRS code for Acton Bridge. <br />
+
+When a valid station name is inputted the program will run through and get the service details and allocation if available from every service at that given location. This is then exported to a database file.<br />
+Details about allocations and which TOC's support this can be found here: https://www.realtimetrains.co.uk/about/knowyourtrain/
+
+# Viewing the results
+After the program finishes the results can be found in the database file it creates for the corresponding station you entered. These can be found in the <code>Databases</code> folder within the project folder. The file should be called <code>The Station Name you inputted.db</code><br />
+To view the data you can use DB Browser for SQLite which is free to download from https://sqlitebrowser.org/dl/. <br />
+Inside the database it should create a table called <code>The Station Name you inputted_Year_Month_Day</code>. In the table it will list all the services and allocations at that Station on that particular date. 
---
+++ RTT_Scraper_Main.py
@@ -0,0 +1,111 @@
+from datetime import date
+from importlib.resources import open_binary
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3 as sl
+
+links = []
+headers = []
+allox = []
+allox_more = []
+n = 9
+m = 21
+
+file_path = "Stations.xlsx"
+df = pd.read_excel(file_path)
+user_station = input("Please enter a station name: ")
+a = df["CRS Code"].where(df["Station Name"] == user_station)
+b = (a.dropna())
+c = b.to_string(index=False)
+
+today = date.today()
+d = today.strftime("%Y-%m-%d")
+
+def no_toc():
+    global URL
+    URL = "https://www.realtimetrains.co.uk/search/detailed/gb-nr:" + c + "/" + d + "/0000-2359?stp=WVS&show=all&order=wtt"
+
+no_toc()
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+for a in soup.find_all('a', href=True):
+    hrefs = a['href']
+    links.append(hrefs)
+
+del links[:n]
+del links[-m:]
+
+for x in range(0, len(links)):
+    formatted_link = ("https://www.realtimetrains.co.uk" + links[x])
+    page_links = requests.get(formatted_link)
+    page_soup = BeautifulSoup(page_links.content, 'html.parser')
+    header = page_soup.find(class_='header')
+    results = page_soup.find(class_='allocation')
+    if results == None:
+        allox.append("No allox")
+        headers.append(header.text)
+    else:
+        changes = len(results.find_all("ul"))
+        if changes == 0:
+            allox.append(results.string)
+            headers.append(header.text)
+        elif changes == 1:
+            for p in results.find_all('li'):
+                allox_more.append(p.text)
+            joined = (" ".join(allox_more))
+            allox.append(joined)
+            headers.append(header.text)
+            allox_more.clear()
+
+
+con = sl.connect("Databases/" + user_station + '.db')
+cursor = con.cursor()
+
+res = " " in user_station
+
+if res is True:
+    table_station = user_station.replace(" ", "_")
+    open_b = "("
+    close_b = ")"
+    if open_b and close_b in table_station:
+        table_station = table_station.replace("(", "")
+        table_station = table_station.replace(")", "")
+    else:
+        pass
+    table_date = d.replace("-", "_")
+    table_name = table_station + "_" + table_date
+else:
+    table_date = d.replace("-", "_")
+    table_name = user_station + "_" + table_date
+
+def insert():
+    for x in range(0, len(links)):
+        sql = "INSERT INTO " + table_name + " (service, allox, date) values(?, ?, ?)"
+        data = [
+            (headers[x], allox[x], d)
+        ]
+        with con:
+            con.executemany(sql, data)
+
+def replace():
+    y = 1
+    for x in range(0 , len(links)):
+        sql = "UPDATE " + table_name + " SET allox = '" + allox[x] + "' WHERE id = " + str(y)
+        with con:
+            con.execute(sql)
+        y += 1
+
+tab_sql = ("SELECT name FROM sqlite_master WHERE type='table' AND name='" + table_name + "'")
+
+cursor.execute(tab_sql)
+table_result = cursor.fetchall()
+
+if table_result:
+    replace()
+else:
+    con.execute("CREATE TABLE " + table_name + " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, service TEXT, allox TEXT, date TEXT)")
+    insert()
